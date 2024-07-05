2e216f27aa3fa8467b0175deeeba5ec0ebff38ce
FacundoEFrias <facufrias@hotmail.com>
2024-07-05 12:27:00 +0200

    Actualizacion del README.md

---
+++ target/generated-sources/annotations/com/mycompany/peluqueriacaninajv/logica/Dueño_.java
@@ -0,0 +1,15 @@
+package com.mycompany.peluqueriacaninajv.logica;
+
+import javax.annotation.processing.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="org.eclipse.persistence.internal.jpa.modelgen.CanonicalModelProcessor", date="2024-07-05T11:39:52", comments="EclipseLink-2.7.10.v20211216-rNA")
+@StaticMetamodel(Dueño.class)
+public class Dueño_ { 
+
+    public static volatile SingularAttribute<Dueño, String> celDuenio;
+    public static volatile SingularAttribute<Dueño, String> nombre;
+    public static volatile SingularAttribute<Dueño, Integer> id_duenio;
+
+}
